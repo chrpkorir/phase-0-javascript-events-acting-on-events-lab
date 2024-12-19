33f7c1e88e9fa0b6eac0d9fa882df60e2fe87406
[chrpkorir] <faythkoko@gmail.com>
2024-12-19 11:38:08 +0300

    Moved things by action on event

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Your code here
+const dodger = document.getElementById("dodger");
+
+function moveDodgerRight() {
+    const left = parseInt(dodger.style.left.replace("px", ""));
+
+    if (left < window.innerWidth - dodger.offsetWidth) {
+        dodger.style.left = `${left + 1}px`;
+}
+}
+
+
+document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerRight();
+    }
+    else if(e.key === "ArrowRight"){
+        moveDodgerRight()
+    }
+});
+
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
